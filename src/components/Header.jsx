d0a45a93371d3a29a75fb944e62da1afeddcde86
abdullah jamshed <abdullahjamshad01@gmail.com>
2020-12-05 14:07:41 +0500

    updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "../styles/Header.module.css";
-import useWebAnimation, { fadeIn, } from "@wellyshen/use-web-animations";
+import useWebAnimation, { fadeIn } from "@wellyshen/use-web-animations";
+import covidIcon from "../images/covidIcon.png";
 
 const Header = () => {
   const { ref } = useWebAnimation({ ...fadeIn });
   return (
     <div ref={ref} className={styles.Header}>
-      <h1>COVID TRACKER APP</h1>
+      <h1>C</h1>
+      <span>
+        <img src={covidIcon} width={40} />
+      </span>
+      <h1>VID TRACKER APP</h1>
     </div>
   );
 };
